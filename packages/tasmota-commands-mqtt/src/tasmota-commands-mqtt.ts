f8307064f9a3334adbf62b1603691fad825c15de
Panu Soisenniemi <panu.soisenniemi@icloud.com>
2022-11-05 11:52:48 +0200

    docs: Fix typo

--- packages/tasmota-commands-mqtt/src/tasmota-commands-mqtt.ts
+++ packages/tasmota-commands-mqtt/src/tasmota-commands-mqtt.ts
@@ -9,7 +9,7 @@ import getMqttCommandHandler, { TasmotaMqttOptions } from './mqtt';
  *    const commands = new TasmotaCommandsMqtt({
  *     host: 'tcp://127.0.0.1',
  *     port: 1883,
- *     topic: 'tasmota_living_roome',
+ *     topic: 'tasmota_living_room',
  *     topicFormat: '%prefix%/%topic%/<command>',
  *     username: 'user',
  *     password: 'password',
