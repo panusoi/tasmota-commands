226c24b8abf62d09608d43a9497ec2fe8c4f0c25
Panu Soisenniemi <panu.soisenniemi@icloud.com>
2022-11-20 20:43:26 +0200

    core: Remove interval test

--- packages/tasmota-commands-core/src/utils/interval.test.ts
+++ packages/tasmota-commands-core/src/utils/interval.test.ts
@@ -2,26 +2,6 @@ import { setAsyncInterval } from './interval';
 
 describe('Interval utils', () => {
   describe('setAsyncInterval', () => {
-    test('should run command every 25 ms', async () => {
-      const start = new Date();
-      let end = new Date();
-      let runs = 0;
-
-      const interval = setAsyncInterval(async () => {
-        if (runs === 0) {
-          end = new Date();
-        }
-        runs++;
-      }, 25);
-
-      await new Promise((r) => setTimeout(r, 100));
-      interval.stop();
-
-      expect(runs).toBeGreaterThanOrEqual(3);
-      expect(end.getTime() - start.getTime()).toBeGreaterThanOrEqual(5);
-      expect(end.getTime() - start.getTime()).toBeLessThanOrEqual(50);
-    });
-
     test('should trigger onStart and onStop', async () => {
       let isStarted = false;
       let isStopped = false;
